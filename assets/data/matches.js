61e2fbc951ff034efd647ef1d4824907986e450e
oxan-jpn <masahira.oxan@gmail.com>
2026-03-17 16:40:53 +0900

    result追加

--- assets/data/matches.js
+++ assets/data/matches.js
@@ -3,36 +3,42 @@ export const matches = [
     "date": "2026-04-05",
     "kickoff": "11:00",
     "opponent": "ドメヌーン",
-    "place": "六郷グラウンド2面"
+    "place": "六郷グラウンド2面",
+    "result": null
   },
   {
     "date": "2026-05-10",
     "kickoff": "09:00",
     "opponent": "池上ドルフィンズ",
-    "place": "六郷グラウンド2面"
+    "place": "六郷グラウンド2面",
+    "result": null
   },
   {
     "date": "2026-05-17",
     "kickoff": "09:20",
     "opponent": "アレグリア",
-    "place": "森ケ崎"
+    "place": "森ケ崎",
+    "result": null
   },
   {
     "date": "2026-05-31",
     "kickoff": "09:00",
     "opponent": "FCベント",
-    "place": "六郷グラウンド1面"
+    "place": "六郷グラウンド1面",
+    "result": null
   },
   {
     "date": "2026-09-27",
     "kickoff": "17:00",
     "opponent": "OL’ERA",
-    "place": "森ケ崎"
+    "place": "森ケ崎",
+    "result": null
   },
   {
     "date": "2026-10-04",
     "kickoff": "08:30",
     "opponent": "池上YS",
-    "place": "六郷グラウンド1面"
+    "place": "六郷グラウンド1面",
+    "result": null
   }
 ];
