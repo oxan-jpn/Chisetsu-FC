345aac1c1441b7be793d47bae6a48f88ed28c26f
oxan-jpn <masahira.oxan@gmail.com>
2026-03-19 22:29:51 +0900

    main.js を更新

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,16 +9,14 @@ function initBackToTop() {
     const backToTop = document.getElementById("back-to-top");
     if (!backToTop) return;
 
-    // 最初は非表示
-    backToTop.style.display = "none";
-
     // スクロール量に応じて表示・非表示
     window.addEventListener("scroll", () => {
         const threshold = window.innerHeight; // ← ウィンドウ高さ100%
+
         if (window.scrollY > threshold) {
-            backToTop.style.display = "block";
+            backToTop.classList.add("show");
         } else {
-            backToTop.style.display = "none";
+            backToTop.classList.remove("show");
         }
     });
 
